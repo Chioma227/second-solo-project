bd26cd0f1e3513fb7e368670e4b82cd5efee1388
Mary <101870828+Chioma227@users.noreply.github.com>
2022-06-24 21:24:49 +0100

    made changes to second project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Header from './components/Header'
 export default function App() {
   return (
     <div>
-      
       <Header />
       <Body />
     </div>
--- src/components/body.js
+++ src/components/body.js
@@ -1,27 +1,37 @@
 import React from 'react'
 import Data from '../data'
 import Main from './Main'
+import { Spring } from 'react-spring'
 
 const Body = () => {
     const datas = Data.map((items) => {
         return (
             <Main
-            key = {items.id}
+                key={items.id}
                 image={items.image}
                 alt={items.alternative}
                 location={items.location}
                 link={items.mapLink}
-                title = {items.title}
-                start = {items.startDate}
-                end = {items.endDate}
-                text = {items.description}
+                title={items.title}
+                start={items.startDate}
+                end={items.endDate}
+                text={items.description}
             />
         )
     })
     return (
-        <div>
-            {datas}
-        </div>
+        <Spring
+        from = {{opacity: 0, marginTop: -500}}
+        to = {{opacity: 1, marginTop: 0}}
+        >
+            {props => (
+                <div style={props}>
+                    <div>
+                        {datas}
+                    </div>
+                </div>
+            )}
+        </Spring>
     )
 }
 
