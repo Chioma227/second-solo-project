7a65df8933b33bd11cff37a3710bc215508771d9
Mary <101870828+Chioma227@users.noreply.github.com>
2022-06-28 22:07:20 +0100

    completed articles in solo project

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,19 +4,22 @@ import Icon from "../image/placeholder.png"
 const Main = (props) => {
     return (
         <div>
-            <section className='d-flex gap-4 align-items-center cards container'>
-                <div>
-                    <img src={props.image} alt = {props.alt} className= "image"/>
+            <section className=' cards container'>
+                <div className='img_div'>
+                    <img src={props.image} alt={props.alt} className="image" />
                 </div>
                 <div className='wrapper'>
-                    <div>
-                        <img src={Icon} alt = "icon"/>
+                <h3 className='title'>{props.title}</h3>
+                    <div className='icon_area'>
+                        <div>
+                            <img src={Icon} alt="icon" />
+                        </div>
                         <span>{props.location}</span>
-                       <a target="_blank" rel='noreferrer' href={props.link} id = "view">view on google map</a>
+                        <a target="_blank" rel='noreferrer' href={props.link} id="view">view on google map</a>
                     </div>
-                    <h3 className='title'>{props.title}</h3>
+                   
                     <small><b>{props.start} - {props.end}</b></small>
-                    <p>{props.text}</p>
+                    <p className='text'>{props.text}</p>
                 </div>
             </section>
         </div>
